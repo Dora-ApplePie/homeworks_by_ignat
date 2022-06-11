5dc767017d9f14acb8dae9f1761cff80c4a9510a
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-06-11 13:01:54 +0300

    (hw 11)  create react input range with 2 values

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,30 +3,53 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(0)
+    const [value2, setValue2] = useState<number>(100)
+
+
+    const onChangeInputRangeHandle = (num: number) => {
+        if (num >= value2) return
+        setValue1(num)
+    }
+
+    const onChangeDoubleInputRangeHandle = (nums: Array<number>) => {
+        setValue1(nums[0])
+        setValue2(nums[1])
+    }
 
     return (
         <div>
-            <hr/>
-            homeworks 11
-
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+            <div style={{width: '60%'}}>
+                <h3>homeworks 11</h3>
+                <br/>
+                <div>
+                    <SuperRange
+                        value={value1}
+                        min={0}
+                        max={100}
+                        bgColor={'#4ac730'}
+                        onChangeRange={onChangeInputRangeHandle}
+                    />
+                    <span>{value1}</span>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <div>
+                    <SuperDoubleRange
+                        value={[value1, value2]}
+                        min={0}
+                        max={100}
+                        onChangeRange={onChangeDoubleInputRangeHandle}
+                        setValue1={setValue1}
+                        setValue2={setValue2}
+                    />
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <span>{value1}</span>
+                        <span>{value2}</span>
+                    </div>
+                </div>
             </div>
-
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
-
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,53 @@
 import React from 'react'
+import SuperRange from '../c7-SuperRange/SuperRange';
+import s from './SuperDoubleRange.module.css'
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    value: [number, number]
+    min: number
+    max: number
+    setValue1: (n: number) => void
+    setValue2: (n: number) => void
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
+        min, max,
+        ...props
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+    const func1 = (n: number) => {
+        if (value[1] <= n) return
+        props.setValue1(n)
+    }
+    const func2 = (n: number) => {
+        if (value[0] >= n) return
+        props.setValue2(n)
+    }
+
+    // --- без типизации переменной атрибут st идёт в ругань :))) при работе с объектом для атрибута style
+    // const st: React.CSSProperties = {
+    //     position: 'absolute',
+    //     width: '100%'
+    // }
 
     return (
-        <>
-            DoubleRange
-        </>
+        <div className={s.double}>
+            <SuperRange value={value[0]} min={min} max={max}
+                        bgColor={'#eee'}
+                        onChangeRange={func1}
+                        styleClassNameRange={s.rangePosition}
+                        styleProgressOverlay={{position: 'relative'}}
+            />
+
+            {/*<SuperRange value={value && value[1]} min={min} max={max} - если опционально*/}
+            <SuperRange value={value[1]} min={min} max={max}
+                        onChangeRange={func2}
+            />
+        </div>
     )
 }
 
